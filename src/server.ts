ee4ae0e6c83c0169746479cb99f84c3555ea0053
jeffsLM <jeffes.dev@gmail.com>
2022-01-16 10:22:31 -0300

    feat: categories Routes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
-import express, { request } from "express";
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello" });
-});
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-    return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running!🚀"));
