3a71446939854dfb17d6c5c0ae0a8c0243c21a46
jeffsLM <jeffes.dev@gmail.com>
2022-01-18 21:22:34 -0300

    refact: modules cars

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,26 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+/**
+ * O SERVICE NUNCA DEVE CONHECER A BASE DADOS
+ */
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private SpecificationRepository: ISpecificationRepository) {}
+
+    execute({ name, description }: IRequest) {
+        const specificationAlreadyExists =
+            this.SpecificationRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error(`Specification ${name} already exists!`);
+        }
+
+        this.SpecificationRepository.create({ name, description });
+    }
+}
+
+export { CreateSpecificationService };
