0c83306c69f0ad20d9cd433498384e16ab3292be
jeffsLM <jeffes.dev@gmail.com>
2022-01-23 19:26:08 -0300

    feat: import csv route

---
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+    constructor(private importCategoryController: ImportCategoryUseCase) {}
+
+    handle(request: Request, response: Response): Response {
+        const { file } = request;
+        this.importCategoryController.execute(file);
+
+        return response.send();
+    }
+}
+
+export { ImportCategoryController };
