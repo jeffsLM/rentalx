ebc8f697ccda31bd0648366e4b8968977e8256e5
jeffsLM <jeffes.dev@gmail.com>
2022-01-23 20:22:55 -0300

    feat: save csv on database

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -36,7 +36,15 @@ class ImportCategoryUseCase {
 
     async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
-        console.log(categories);
+
+        categories.map(async (category) => {
+            const { name, description } = category;
+            const existsCategory = this.categoryRepository.findByName(name);
+
+            if (!existsCategory) {
+                this.categoryRepository.create({ name, description });
+            }
+        });
     }
 }
 
